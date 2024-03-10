02beddd7180ec544609d74f6d6f5bf84a2ce2af1
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2024-03-10 10:26:07 -0300

    Refactored App.vue layout with header, content, and footer sections

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">
-      <a-button type="primary">Home</a-button>
-    </router-link>
-    <a-divider type="vertical" />
-    <router-link to="/ready">
-      <a-button type="primary">Phone</a-button>
-    </router-link>
-  </nav>
-  <router-view />
+  <a-layout>
+    <a-layout-header>
+      <a-row style="text-align: center">
+        <a-col :span="24">
+          <nav>
+            <router-link to="/">
+              <a-button type="primary">Home</a-button>
+            </router-link>
+            <a-divider type="vertical" />
+            <router-link to="/ready">
+              <a-button type="primary">Phone</a-button>
+            </router-link>
+          </nav>
+        </a-col>
+      </a-row>
+    </a-layout-header>
+    <a-layout-content class="content">
+      <router-view />
+    </a-layout-content>
+    <a-layout-footer class="footer">
+      <a-divider />
+      <a-typography-paragraph>InPhone - Dev Challenge</a-typography-paragraph>
+    </a-layout-footer>
+  </a-layout>
 </template>
 
 <style lang="scss">
@@ -16,8 +30,14 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+
+.footer {
+  text-align: center;
+}
+.content {
+  padding-top: 32px;
 }
 
 nav {
