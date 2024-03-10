c86f25047439b05da5647b5748d312723ff54849
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2024-03-10 08:16:44 -0300

    Added WaitingCall component to ReadyView.vue

---
+++ src/views/ReadyView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <WaitingCall />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import WaitingCall from "@/components/WaitingCall.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    WaitingCall,
+  },
+})
+export default class ReadyView extends Vue {}
+</script>
