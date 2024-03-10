68502736d68e0003f361dd763ba2e90fccc0a45f
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2024-03-10 08:16:10 -0300

    Added new route '/ready' with ReadyView component to router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ReadyView from "@/views/ReadyView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/ready",
+    name: "ready",
+    component: ReadyView,
   },
   {
     path: "/about",
