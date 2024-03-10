ffe955e686d6669cc41b062f697ad1f01a73d147
Erick Engelhardt <erick.engelhardt@rockapps.com.br>
2024-03-10 10:25:32 -0300

    Added TimelineComponent and TimelineInterface to display timeline data in Vue

---
+++ src/components/Timeline/TimelineComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-space direction="vertical" class="left-aligned-items">
+    <span v-for="(item, index) in timelineData" :key="index">
+      <a-badge :status="item.type || 'default'" :text="format(item)" />
+    </span>
+  </a-space>
+</template>
+
+<style scoped>
+.left-aligned-items .ant-space-item {
+  text-align: left !important;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { TimelineItem } from "@/components/Timeline/TimelineInterface.vue";
+
+function format(item: TimelineItem): string {
+  return item.date.format("YYYY-MM-DD HH:mm:ss") + " " + item.message;
+}
+export default defineComponent({
+  name: "TimelineComponent",
+  props: {
+    timelineData: {
+      type: Array as () => TimelineItem[],
+      required: true,
+    },
+  },
+  methods: {
+    format,
+  },
+});
+</script>
---
+++ src/components/Timeline/TimelineInterface.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { Moment } from "moment";
+
+export type TimelineTypes =
+  | "error"
+  | "success"
+  | "default"
+  | "processing"
+  | "warning";
+export interface TimelineItem {
+  date: Moment;
+  message: string;
+  type: TimelineTypes;
+}
+</script>
